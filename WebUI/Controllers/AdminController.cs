dcb83dcd271475b1916e906d3b4e03be6e93be6b
Evgeny Kurmysa <zhenya.kurmysa@gmail.com>
2021-11-21 15:53:04 +0300

    Дополнительные улучшения UI, добавлен контроллер для админки

---
+++ WebUI/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Domain.Abstract;
+
+namespace WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        readonly IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
